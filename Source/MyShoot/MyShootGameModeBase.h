6d46ebe5d09c930a656e60b7bde29352328c0904
ohcumt <1040183251@qq.com>
2017-03-09 13:34:13 +0800

    myshoot

---
+++ Source/MyShoot/MyShootGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MyShootGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYSHOOT_API AMyShootGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
